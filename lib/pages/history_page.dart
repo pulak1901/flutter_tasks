4fd94fac63da3a4b5116d6ec0005490aba25a738
Pulak Kumar Das <pulak1901@gmail.com>
2022-01-26 12:30:05 +0530

    Update history page to display completed todos

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tasks/data/todo.dart';
+import 'package:flutter_tasks/data/todo_list_model.dart';
+import 'package:provider/provider.dart';
 
 class HistoryPage extends StatelessWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Text("History");
+    return Selector<TodoListModel, List<Todo>>(
+        selector: (_, model) => model.completedTodos,
+        builder: (context, todos, _) {
+          todos.sort((t1, t2) => DateTime.parse(t2.completed)
+              .compareTo(DateTime.parse(t1.completed)));
+          return ListView.builder(
+            itemCount: todos.length,
+            itemBuilder: (context, i) => ListTile(
+              key: Key(i.toString()),
+              title: Text(todos[i].description),
+              subtitle: Text(todos[i].completed),
+            ),
+          );
+        });
   }
 }
